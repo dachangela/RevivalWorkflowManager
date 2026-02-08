282ba9a48363a8efa1b9f42185d259ac289f9586
Dylan Changela <dylan@changelas.com>
2026-02-08 11:44:21 -0500

    Uncommenting the security for API requests

--- workflow.manager/backend/src/main/java/com/revival/workflow/manager/config/SecurityConfig.java
+++ workflow.manager/backend/src/main/java/com/revival/workflow/manager/config/SecurityConfig.java
@@ -1,41 +1,41 @@
-// package com.revival.workflow.manager.config;
+package com.revival.workflow.manager.config;
 
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.security.config.Customizer;
-// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-// import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
-// @Configuration
-// public class SecurityConfig {
+@Configuration
+public class SecurityConfig {
 
-//     @Bean
-//     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//         http.csrf(csrf -> csrf.disable())
-//             .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-//             .httpBasic(Customizer.withDefaults());
-//         return http.build();
-//     }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
-//     @Bean
-//     public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
-//         String username = System.getenv().getOrDefault("API_USER","apiuser");
-//         String password = System.getenv().getOrDefault("API_PASS","changeme");
-//         UserDetails user = User.withUsername(username)
-//             .password(encoder.encode(password))
-//             .roles("USER")
-//             .build();
-//         return new InMemoryUserDetailsManager(user);
-//     }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+        String username = System.getenv().getOrDefault("API_USER","apiuser");
+        String password = System.getenv().getOrDefault("API_PASS","changeme");
+        UserDetails user = User.withUsername(username)
+            .password(encoder.encode(password))
+            .roles("USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
-//     @Bean
-//     public PasswordEncoder passwordEncoder() {
-//         return new BCryptPasswordEncoder();
-//     }
-// }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
